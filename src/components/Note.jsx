b446ce968417f14ca0a7a31c4f60e60553917cee
Ochwada <nakhulolinda@yahoo.com>
2023-02-03 01:28:04 +0100

    create and link components

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div>
+            <h1> The Title</h1>
+            <p> This is the content</p>
+        </div>
+    )
+}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-import Header from './Header';
-import Footer from './Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +11,9 @@ root.render(
     <div>
         <div>
             <Header />
+        </div>
+        <div>
+            <Note />
         </div>
         <div>
             <Footer />
