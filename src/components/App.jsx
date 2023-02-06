f52dd6b69bff1a26b236e815ee366385768b2925
Ochwada <nakhulolinda@yahoo.com>
2023-02-06 20:47:30 +0100

    Map-filter-reduce

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes";
+
+
+function createNotes(noteItem) {
+    return <Note
+        title={noteItem.title}
+        content={noteItem.content}
+    />
+}
 
 function App() {
-    return <div>
-        <Header />
-        <Note/>
-        <Footer/>
-    </div>
+    return (
+        <div>
+            <Header />
+            {notes.map(createNotes)}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
